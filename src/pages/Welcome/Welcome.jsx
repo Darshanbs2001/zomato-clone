f560e861a96b882cbd6770b1100581cc6c84eb09
Darshanbs2001 <darshandarsha59@gmail.com>
2023-12-24 18:24:39 +0530

    this is the second day commit

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,34 +1,13 @@
 import {Link} from 'react-router-dom';
 import bgimg from '../../assets/front-page-image.avif'
 import './welcome.css';
+import { Navbar } from '../../components/Navbar/Navbar';
 const Welcome = () => {
   return (
-    <div class='Welcome'>
-
-      
-     <nav class='navigation-bar'>
-     <div className="bg-img">
-        <img src={bgimg} alt="" />
-      </div>
-      <div className="header">  
-      <div className="left"></div>
-      <ul className="right">
-        <li>
-          <Link to='/Investor-relations'>Investor Relations</Link>
-        </li>
-        <li>
-          <Link to='/Add-restaurant'>Add Restaurant</Link>
-        </li>
-        <li>
-          <Link to='/Login'>Log In</Link>
-        </li>
-        <li>
-          <Link to='/Signup'>Sign In</Link>
-        </li>
-      </ul>
-      </div>
- 
-      </nav>    
+    <div>
+    <header>
+    <Navbar/>
+    </header>
     </div>
   )
 }
